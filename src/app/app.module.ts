3617fa7363024cbf33c3a19d48dc3950bcf98f74
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2017-11-10 17:30:31 +0530

    added wildcards and outsourced routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
-import { RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -17,34 +16,8 @@ import { SillserviceService } from './sillservice.service';
 import { SkillviewComponent } from './skilist/skillview/skillview.component';
 import { SkilleditComponent } from './skilist/skilledit/skilledit.component';
 import { SkilltestComponent } from './skilist/skilltest/skilltest.component';
-
-
-const myrouteurls :Routes = [
-
-  {path:'',component:HomeComponent},
-  // {path:'', redirectTo:'services', pathMatch:'full'},
-  {path:'services',component:ServicesComponent},
-  {path:'services/:cid',component:ServicesComponent},
-  {path:'services/:cid/:cname',component:ServicesComponent},
-  {path:'services/:cid/:cname/:cfee',component:ServicesComponent},
-
-  {path:'route',component:RouteComponent},
-  {path:'route/:cid',component:RouteComponent},
-  {path:'route/:cid/:cname',component:RouteComponent},
-  {path:'route/:cid/:cname/:cfee',component:RouteComponent},
-
-  {path:'skill',component:SkillComponent},
-  {path:'skillist',component:SkilistComponent, children:[
-    {path:':name',component:SkillviewComponent},
-    {path:'edit/:name',component:SkilleditComponent},
-    {path:'view/:name',component:SkilltestComponent}
-  ]}
-  // {path:'skillist/:name',component:SkilistComponent},
-  // {path:'skillist/:name/:sname',component:SkilistComponent},
-  // {path:'skillist/:name/:sname/:srate',component:SkilistComponent},
-  // {path:'skillist/:name/:sname/:srate/:sdes',component:SkilistComponent}
-];
-
+import { AppRoutesModule } from './approutes.module';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
@@ -60,11 +33,12 @@ const myrouteurls :Routes = [
     SkillviewComponent,
     SkilleditComponent,
     SkilltestComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(myrouteurls)
+    AppRoutesModule
   ],
   providers: [OneserviceService,SillserviceService],
   bootstrap: [AppComponent]
---
+++ src/app/approutes.module.ts
@@ -0,0 +1,48 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+
+import {SkillComponent} from "./skill/skill.component";
+import {RouteComponent} from "./route/route.component";
+import {HomeComponent} from "./home/home.component";
+import {ServicesComponent} from "./services/services.component";
+import {SkilistComponent} from "./skilist/skilist.component";
+import {SkillviewComponent} from "./skilist/skillview/skillview.component";
+import {SkilleditComponent} from "./skilist/skilledit/skilledit.component";
+import {SkilltestComponent} from "./skilist/skilltest/skilltest.component";
+import {NotfoundComponent} from "./notfound/notfound.component";
+
+const myrouteurls : Routes = [
+  {path:'',component:HomeComponent},
+  // {path:'', redirectTo:'services', pathMatch:'full'},
+  {path:'services',component:ServicesComponent},
+  {path:'services/:cid',component:ServicesComponent},
+  {path:'services/:cid/:cname',component:ServicesComponent},
+  {path:'services/:cid/:cname/:cfee',component:ServicesComponent},
+
+  {path:'route',component:RouteComponent},
+  {path:'route/:cid',component:RouteComponent},
+  {path:'route/:cid/:cname',component:RouteComponent},
+  {path:'route/:cid/:cname/:cfee',component:RouteComponent},
+
+  {path:'skill',component:SkillComponent},
+  {path:'skillist',component:SkilistComponent, children:[
+    {path:':name',component:SkillviewComponent},
+    {path:'edit/:name',component:SkilleditComponent},
+    {path:'view/:name',component:SkilltestComponent}
+  ]},
+  {path:'error', component: NotfoundComponent},
+  {path:'**', redirectTo:'error'}
+  // {path:'skillist/:name',component:SkilistComponent},
+  // {path:'skillist/:name/:sname',component:SkilistComponent},
+  // {path:'skillist/:name/:sname/:srate',component:SkilistComponent},
+  // {path:'skillist/:name/:sname/:srate/:sdes',component:SkilistComponent}
+];
+
+@NgModule({
+  imports:[
+    RouterModule.forRoot(myrouteurls)
+  ],
+  exports:[RouterModule]
+})
+
+export class AppRoutesModule{ }
