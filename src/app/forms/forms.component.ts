a2c1069856fd622d71c560f9cf2a2e41d852e234
Teja Varma <tejavarma@Tejas-MacBook-Pro.local>
2017-12-04 16:36:00 +0530

    template driven forms complete

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -8,8 +8,13 @@ import { ViewChild } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
   @ViewChild('myform') mynewform;
+  @ViewChild('refEmail2') email2;
   custEmail = false;
   pass = "";
+  emailValidate = false;
+  success = false;
+  myoptions = 2;
+
   constructor() { }
 
   ngOnInit() {
@@ -21,15 +26,55 @@ export class FormsComponent implements OnInit {
   //   console.log(formdata);
   // }
 
+  filluptheform(){
+    // this.mynewform.setValue({
+    //   mygroup:{
+    //     email:"kvarma063@gmail.com",
+    //     password:"testing"
+    //   },
+    //   options:2,
+    //   description:"checking",
+    //   email2:"kvarma@gmail.com"
+    // });
+
+    this.mynewform.form.setValue({
+      mygroup:{
+        email:"kvarma063@gmail.com",
+        password:"testing"
+      },
+      options:2,
+      description:"checking",
+      email2:"kvarma@gmail.com"
+    });
+
+    // this.mynewform.form.patchValue({
+    //   mygroup:{
+    //     email:"kvarma063@gmail.com",
+    //     password:"testing"
+    //   }
+    // });
+  }
+
   submitForm(){
-    if(this.mynewform.value.email===""){
+    if(this.mynewform.value.mygroup.email===""){
       this.custEmail = true;
     }
 
-    //console.log(this.mynewform);
+    console.log(this.mynewform);
   }
   checkpass(){
     this.pass = this.mynewform.value.password
+  }
+
+  checkEmail(){
+    //minimum letters > 5&&pattern not matched {    this.emailValidate = true; this.success = false }
+    //this.emailValidate = false; this.success = true
+    this.emailValidate = true;
+    console.log(this.email2.value);
+  }
+
+  resetmyfrom(){
+    this.mynewform.reset();
   }
 
 }
